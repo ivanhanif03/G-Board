ef8413bd0dfe465fb1d0a6c6af5178be79e0d873
M Ivan Hanif V <ivanhanif03@gmail.com>
2022-06-30 14:22:13 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    const preloader = document.getElementById('preloader');
+
+    preloader.style.display = 'none';
+}, 4000);
+
+setTimeout(() => {
+    const loginPage = document.getElementById('loginPage');
+
+    loginPage.style.display = 'block';
+}, 4000);
